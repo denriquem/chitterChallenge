ed2cddb9f8b12f68eec721662a94b5cf15042f6b
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-10-11 17:39:34 +0100

    card component complete

--- src/components/Peeps.js
+++ src/components/Peeps.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import classes from "./Peeps.module.css";
+import Card from "./Card";
 
 const Peeps = () => {
 	const [peeps, setPeeps] = useState([]);
@@ -10,16 +12,26 @@ const Peeps = () => {
 				const response = await axios.get(
 					"https://chitter-backend-api-v2.herokuapp.com/peeps"
 				);
-				console.log(response.data);
+				console.log("hello there");
+				let loadedPeeps = response.data;
+				setPeeps(loadedPeeps);
+
+				console.log(peeps);
 			} catch (error) {
 				console.log(error);
 			}
 		};
 
 		fetchPeeps();
-	});
+	}, []);
 
-	return <div>These will be the Peeps</div>;
+	return (
+		<section className={classes.peeps}>
+			<Card>
+				<div>Here Goeth The Peeps</div>
+			</Card>
+		</section>
+	);
 };
 
 export default Peeps;
